3ba2ea406b78dc3dbe692ae47b888770a6865ebc
robpark04 <robinsonstewart0428@gmail.com>
2022-01-13 21:53:27 -0500

    bug fix

---
+++ src/routes/index.tsx
@@ -0,0 +1,21 @@
+import { useRoutes } from 'react-router-dom';
+
+// import { Landing } from '@/features/misc';
+import { Landing } from '@/features/landing';
+import { useAuth } from '@/lib/auth';
+
+import { protectedRoutes } from './protected';
+import { publicRoutes } from './public';
+
+export const AppRoutes = () => {
+  const auth = useAuth();
+
+  const commonRoutes = [{ path: '/', element: <Landing /> }];
+
+  const routes = auth.user ? protectedRoutes : publicRoutes;
+
+  // const element = useRoutes([...routes, ...commonRoutes]);
+  const element = useRoutes([...commonRoutes]);
+
+  return <>{element}</>;
+};
